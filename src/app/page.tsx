98e4000468c369d1a516679ac3c5c0072d049d99
AsherDoesTechs <Asherbasco93@gmail.com>
2025-06-18 19:20:06 +0800

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,3 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 import Script from "next/script";
+
+export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      require("/public/script.js");
+    } 
+}, []); 
