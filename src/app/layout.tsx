1ec06bd801423c52706ee647551a0f6007bab84f
AsherDoesTechs <Asherbasco93@gmail.com>
2026-03-11 11:51:37 +0800

    Added Analytics Provided by Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Asher Basco - Personal Portfolio",
@@ -19,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body
         className={`${inter.className} bg-gradient-to-b from-gray-50 via-white to-gray-50 text-gray-900`}
       >
